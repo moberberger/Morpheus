29d624ba30647452050e57c3c7defdc60ca37f1c
Michael Oberberger <52931052+moberberger@users.noreply.github.com>
2022-01-12 09:17:05 -0600

    Documentation for FluentExtensions

--- Standard/src/ExtensionMethods/FluentExtensions.cs
+++ Standard/src/ExtensionMethods/FluentExtensions.cs
@@ -8,6 +8,13 @@ namespace Morpheus
 {
     public static class FluentExtensions
     {
+        /// <summary>
+        /// Change the "current" object, but make sure that if the current object is enumerable,
+        /// that enumeration is "run" using a foreach statement
+        /// </summary>
+        /// <typeparam name="T">The new type for Current</typeparam>
+        /// <typeparam name="U">The type of "current"</typeparam>
+        /// <returns>A new object as the "current" fluent object</returns>
         public static T NowUse<T, U>( this U current, T newObj )
         {
             if (current is IEnumerable && !(
@@ -20,20 +27,39 @@ namespace Morpheus
 
             return newObj;
         }
+
+        /// <summary>
+        /// Fluent form of canonical condition
+        /// </summary>
         public static T If<T>( this bool condition, Func<T> trueAction, Func<T> falseAction ) =>
             condition
                 ? trueAction()
                 : falseAction();
 
+        /// <summary>
+        /// Execute an operation on "current", but return "current" as the operation has no
+        /// return value.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="operation"></param>
+        /// <returns></returns>
         public static T With<T>( this T obj, Action<T> operation )
         {
             operation( obj );
             return obj;
         }
 
+        /// <summary>
+        /// <see cref="Enumerable.Select{}"/> but for objects instead of enumerations
+        /// </summary>
         public static T Translate<T, U>( this U obj, Func<U, T> operation ) =>
             operation( obj );
 
+        /// <summary>
+        /// Thin wrapper around <see cref="Enumerable.Range(int, int)"/> that only considers one
+        /// value (the max value), relying on optimizations found in the Enumerable version
+        /// </summary>
         public static IEnumerable<int> AsRange( this int max ) => Enumerable.Range( 0, max );
     }
 }
