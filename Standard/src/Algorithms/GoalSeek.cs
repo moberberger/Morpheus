02365dea0380493f0c2bbd4fa413d8afabfd7b7f
Michael Oberberger <52931052+moberberger@users.noreply.github.com>
2023-06-19 12:00:24 -0700

    Optimized GoalSeek.Simple to only throw away one function() call IFF the function is inverted.

--- Standard/src/Algorithms/GoalSeek.cs
+++ Standard/src/Algorithms/GoalSeek.cs
@@ -6,19 +6,57 @@ public class GoalSeek
     {
         double low = Math.Min( minimum, maximum );
         double high = Math.Max( minimum, maximum );
-        double step = (high - low) / 3;
-        double left = low + step;
-        double right = low + step * 2;
 
-        double leftX = function( left );
-        double rightX = function( right );
-        double direction = Math.Sign( rightX - leftX );
+        double x = (high + low) / 2;
+        double fnx = function( x );
 
+        // Set up for a second "probing" function() call to see if function is inverted
+        double low2, high2;
+        if (fnx < target)
+        {
+            low2 = x;
+            high2 = high;
+        }
+        else
+        {
+            low2 = low;
+            high2 = x;
+        }
+
+        double x2 = (high2 + low2) / 2;
+        double fnx2 = function( x2 );
+
+        // Test to see if the function is inverted. If it is, then the first comparison /
+        // setting of high and low are invalid, and need to be reversed. If it is not inverted,
+        // then use the output of the probing function() call to set the new parameters for
+        // high/low.
+        double direction;
+        if ((fnx < target && fnx2 > fnx) || (fnx > target && fnx2 < fnx))
+        {
+            direction = 1; // The function() is not inverted
+            if (fnx2 < target) // fnx2 is valid
+            {
+                low = x2;
+                high = high2;
+            }
+            else
+            {
+                low = low2;
+                high = x2;
+            }
+        }
+        else // inverted... fnx2 is invalid
+        {
+            direction = -1;
+            if (fnx < target) // the opposite of what we did up top
+                high = x;
+            else
+                low = x;
+        }
+
+        // Now proceed with a loop now that we know -direction-
         double dTarget = target * direction;
-        double x, lastX = double.NaN;
-
-
-
+        double lastX = double.NaN;
         for (int i = 0; i < 1000; i++)
         {
             x = (high + low) / 2;
@@ -26,12 +64,14 @@ public class GoalSeek
                 return x; // good as it gets with a double precision number
             lastX = x;
 
-            double fnx = function( x ) * direction;
+            fnx = function( x ) * direction;
             if (fnx < dTarget)
                 low = x;
             else
                 high = x;
         }
+
+        // should never happen
         throw new InvalidProgramException( "This should never happen, as the precision of a double should make the loop " +
             "terminate within 56-57 iterations" );
     }
