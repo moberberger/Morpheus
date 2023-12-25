2910beb166e587d4cd6682466ad6bb144a60eb77
Michael Oberberger <52931052+moberberger@users.noreply.github.com>
2023-12-25 09:48:26 -0600

    Added ObjectPropertyList to Morpheus and used in ReelstripEditor to display the PaytableSummary

---
+++ Standard/src/Miscellaneous/ObjectPropertyList.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace Morpheus;
+
+public class ObjectPropertyList : CSortableBindingList<ObjectPropertyList.Accessor>
+{
+    public class Order : Attribute
+    {
+        public int OrderValue { get; }
+        /// <summary>
+        /// All members with no <see cref="Order"/> attribute are sorted at value 0.
+        /// </summary>
+        public Order( int orderValue ) => OrderValue = orderValue;
+    }
+
+    public class Ignore : Attribute { }
+
+    public class Accessor
+    {
+        private PropertyInfo PropertyInfo { get; }
+        private object TheObject { get; }
+        internal int Order { get; } = 0;
+
+        public Accessor( PropertyInfo pi, object theObject )
+        {
+            PropertyInfo = pi;
+            TheObject = theObject;
+            var orderAttr = pi.GetCustomAttribute<Order>();
+            if (orderAttr != null)
+                Order = orderAttr.OrderValue;
+        }
+        public string Name => PropertyInfo.Name;
+        public string Value
+        {
+            get => PropertyInfo.GetValue( TheObject )?.ToString() ?? "";
+            set => PropertyInfo.SetValue( TheObject, Convert.ChangeType( value, PropertyInfo.PropertyType ) );
+        }
+    }
+
+    public ObjectPropertyList( object obj ) =>
+        AddRange(
+            obj.GetType()
+            .GetProperties( BindingFlags.Public | BindingFlags.Instance )
+            .Where( pi => pi.PropertyType.IsPrimitive || pi.PropertyType == typeof( string ) )
+            .Where( pi => pi.GetCustomAttribute<Ignore>() is null )
+            .Select( pi => new Accessor( pi, obj ) )
+            .OrderBy( acc => acc.Order )
+        );
+}
