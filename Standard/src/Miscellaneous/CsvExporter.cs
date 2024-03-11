9092c440eb47e57511a046044b148ee3dec959c0
Michael Oberberger <52931052+moberberger@users.noreply.github.com>
2024-03-11 10:54:37 -0500

    Added CsvExporter to handle enumerations of objects as an extension method

---
+++ Standard/src/Miscellaneous/CsvExporter.cs
@@ -0,0 +1,41 @@
+﻿namespace Morpheus;
+
+public static class CsvExporter
+{
+    public static string ToCsv<T>( this IEnumerable<T> values, int maxLen = -1 ) where T : class
+    {
+        var sb = new StringBuilder();
+        var props = typeof( T ).GetProperties();
+        var header = string.Join( ",", props.Select( p => p.Name ) );
+        sb.AppendLine( header );
+
+        foreach (var value in values)
+        {
+            var line = props
+                .Select( p => p.GetValue( value ) ?? "" )
+                .Select( v => EscapeForCsv( v, maxLen ) )
+                .JoinAsString( "," );
+
+            sb.AppendLine( line );
+        }
+
+        return sb.ToString();
+    }
+
+    public static string EscapeForCsv( object inputObject, int maxLen )
+    {
+        string input = inputObject.ToString()
+            ?? throw new WhyIsThisNullException( "inputObject.ToString() returned null" );
+
+        if (maxLen > 0 && input.Length > maxLen)
+            input = input[..maxLen];
+
+        if (input.Contains( "," ) || input.Contains( "\"" ) || input.Contains( "\n" ) || input.Contains( "\r" ))
+        {
+            input = input.Replace( "\"", "\"\"" );
+            input = $"\"{input}\"";
+        }
+
+        return input;
+    }
+}
