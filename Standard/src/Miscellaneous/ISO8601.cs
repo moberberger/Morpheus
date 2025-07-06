69dcede5253480b93cef1690b9a141acbea9bf8b
Michael Oberberger <52931052+moberberger@users.noreply.github.com>
2025-07-06 14:54:07 -0700

    Added ISO8601 wrapper for dates

---
+++ Standard/src/Miscellaneous/ISO8601.cs
@@ -0,0 +1,10 @@
+﻿namespace Morpheus;
+
+public static class ISO8601
+{
+    public static string ToIso8601( DateTime dateTime ) =>
+        dateTime.ToUniversalTime().ToString( "yyyy-MM-dd'T'HH:mm:ss'Z'" );
+
+    public static DateTime FromIso8601( string iso8601 ) =>
+        DateTime.Parse( iso8601 );
+}
